ae59f747faa63267fbc31a06958933e9703bd2b0
InsuHam0315 <gkadlstn1@gmail.com>
2025-12-16 19:56:28 +0900

    12/15 변경사항 정리

--- Documents/카카오톡 받은 파일/DevOpsDatabaseProject-main_v1/DevOpsDatabaseProject-main_v1/frontend/app/dashboard/page.tsx
+++ Documents/카카오톡 받은 파일/DevOpsDatabaseProject-main_v1/DevOpsDatabaseProject-main_v1/frontend/app/dashboard/page.tsx
@@ -43,7 +43,8 @@ const formatRunDate = (value?: string) => {
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
-    minute: '2-digit'
+    minute: '2-digit',
+    second: '2-digit'
   });
 };
 
